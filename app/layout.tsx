ab4391890e9b8923317016293c24af3abe160e04
satyam kumar <satyarajbarnwal2008@gmail.com>
2026-04-20 17:47:34 +0530

    meta data update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Profile-io",
+  description: "Best way to share your profile",
 };
 
 const geistSans = Geist({
